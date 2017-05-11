0062fed006dfe4de3dc0c51335deecc38e356d9e
meijer <wkmeijer@users.noreply.github.com>
2017-05-11 14:43:49 +0200

    added code from epassport app so an activity can be loaded that reads data from the passport

--- app/src/main/java/com/digitalvotingpass/digitalvotingpass/MainActivity.java
+++ app/src/main/java/com/digitalvotingpass/digitalvotingpass/MainActivity.java
@@ -1,7 +1,9 @@
 package com.digitalvotingpass.digitalvotingpass;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the "Start Reading ID" button */
+    public void startReading(View view) {
+        Intent intent = new Intent(this, jMRTDActivity.class);
+        startActivity(intent);
     }
 }
