f691fb58317c3182c842feffef6c7af3d19ff0e8
Jonathan Raes <jonathanraes@gmail.com>
2017-05-11 11:20:20 +0200

    google vision to read text from camera feed and place overlay
    taken from google example; https://codelabs.developers.google.com/codelabs/mobile-vision-ocr/#0

--- app/src/main/java/com/digitalvotingpass/digitalvotingpass/MainActivity.java
+++ app/src/main/java/com/digitalvotingpass/digitalvotingpass/MainActivity.java
@@ -1,13 +1,42 @@
 package com.digitalvotingpass.digitalvotingpass;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.text.TextRecognizer;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_OCR_CAPTURE = 9003;
+
+    private Button startOCR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final MainActivity mainActivity = this;
         setContentView(R.layout.activity_main);
+
+        startOCR = (Button) findViewById(R.id.button_start_ocr);
+
+        startOCR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mainActivity, OcrCaptureActivity.class);
+                intent.putExtra(OcrCaptureActivity.AutoFocus, true/*autoFocus.isChecked()*/);
+                intent.putExtra(OcrCaptureActivity.UseFlash, false/*useFlash.isChecked()*/);
+
+                startActivityForResult(intent, RC_OCR_CAPTURE);
+            }
+        });
     }
+
 }
