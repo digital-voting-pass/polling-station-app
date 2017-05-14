0ac72b49e31e3123599c7a50b5f76bfdeb91edd8
Jonathan Raes <jonathanraes@gmail.com>
2017-05-14 16:38:45 +0200

    OCR scanning with tesseract prototype
    Start scan by clicking take picture.  While scanning cropped image is shown in the bordered area. Cropping is buggy but it works. horizontal mode is currently broken

--- app/src/main/java/com/digitalvotingpass/digitalvotingpass/MainActivity.java
+++ app/src/main/java/com/digitalvotingpass/digitalvotingpass/MainActivity.java
@@ -1,13 +1,28 @@
 package com.digitalvotingpass.digitalvotingpass;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startOCR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final MainActivity thisActivity = this;
         setContentView(R.layout.activity_main);
+
+        startOCR = (Button) findViewById(R.id.start_ocr);
+        startOCR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(thisActivity, CameraActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
