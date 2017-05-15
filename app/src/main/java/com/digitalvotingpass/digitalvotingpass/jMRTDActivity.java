33aab6b1c0c0d728ce781dec9205e58ac54e5168
meijer <wkmeijer@users.noreply.github.com>
2017-05-15 11:52:00 +0200

    Cleaned up code

--- app/src/main/java/com/digitalvotingpass/digitalvotingpass/jMRTDActivity.java
+++ app/src/main/java/com/digitalvotingpass/digitalvotingpass/jMRTDActivity.java
@@ -18,7 +18,6 @@ import net.sf.scuba.smartcards.CardService;
 import net.sf.scuba.smartcards.CardServiceException;
 import net.sf.scuba.tlv.TLVOutputStream;
 
-
 import org.jmrtd.BACKeySpec;
 import org.jmrtd.ChipAuthenticationResult;
 import org.jmrtd.DESedeSecureMessagingWrapper;
@@ -58,36 +57,41 @@ public class jMRTDActivity extends AppCompatActivity {
     static {
         Security.addProvider(new BouncyCastleProvider());
     }
+    // Adapter for NFC connection
     private NfcAdapter mNfcAdapter;
 
+
+    /**
+     * This activity usually be loaded from the starting screen of the app.
+     * This method handles the start-up of the activity, it does not need to call any other methods
+     * since the activity onNewIntent() calls the intentHandler when a NFC chip is detected.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         Intent intent = getIntent();
+
         setContentView(R.layout.activity_j_mrtd);
         TextView textView = (TextView) findViewById(R.id.textView);
 
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
         if (mNfcAdapter == null) {
             // Stop here, we definitely need NFC
-            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            textView.setText(R.string.nfc_not_supported_error);
+            Toast.makeText(this, R.string.nfc_not_supported_error, Toast.LENGTH_LONG).show();
             finish();
             return;
         }
-
         if (!mNfcAdapter.isEnabled()) {
-            textView.setText("NFC is disabled.");
+            textView.setText(R.string.nfc_disabled_error);
         }
-
-        handleIntent(intent);
     }
 
-    private void testIntentHandler(Intent intent ){
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText("intent is handled");
-    }
-
+    /**
+     * Some methods to ensure that when the activity is opened the ID is read.
+     * When the activity is opened any nfc device held against the phone will cause, handleIntent to
+     * be called.
+     */
     @Override
     protected void onResume() {
         super.onResume();
@@ -150,16 +154,64 @@ public class jMRTDActivity extends AppCompatActivity {
         adapter.disableForegroundDispatch(activity);
     }
 
+    /**
+     * Handle the intent following from a NFC detection.
+     *
+     */
     private void handleIntent(Intent intent){
         TextView textView = (TextView) findViewById(R.id.textView);
         TextView textSignedData = (TextView) findViewById(R.id.signedData);
 
+        // if nfc tag holds no data, return
         Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
         if (tag == null) {
             return;
-        } else {
         }
 
+        // Open a connection with the ID, return a PassportService object which holds the open connection
+        PassportService ps = openConnection(tag);
+        InputStream is = null;
+        InputStream is15 = null;
+        try {
+            // Get the BSN from datagroup1 to confirm the ID was scanned correctly.
+            // This is for testing purposes
+            is = ps.getInputStream(PassportService.EF_DG1);
+            DG1File dg1 = (DG1File) LDSFileUtil.getLDSFile(PassportService.EF_DG1, is);
+            Toast.makeText(this, dg1.getMRZInfo().getPersonalNumber(), Toast.LENGTH_LONG).show();
+
+            // display data from dg15
+            is15 = ps.getInputStream(PassportService.EF_DG15);
+
+            PublicKey pubk = getAAPublicKey(ps,is15);
+            String publicKey = new BigInteger(pubk.getEncoded()).toString(2);
+            int length = pubk.getEncoded().length;
+            textView.setText(Integer.toString(length));
+
+            // sign 8 bytes of data and display the signed data
+            textSignedData.setText(byteArrayToHexString(signData(ps,is15,pubk)));
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            Toast.makeText(this, R.string.general_error, Toast.LENGTH_LONG).show();
+        } finally {
+            try {
+                ps.close();
+                is.close();
+                is15.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(this, R.string.general_error, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    /**
+     * Opens a connection with the ID by doing BAC
+     * Uses hardcoded parameters for now
+     * @param tag - NFC tag that started this activity (ID NFC tag)
+     * @return PassportService - passportservice that has an open connection with the ID
+     */
+    private PassportService openConnection(Tag tag) {
         PassportService ps = null;
         try {
             IsoDep nfc = IsoDep.get(tag);
@@ -167,265 +219,64 @@ public class jMRTDActivity extends AppCompatActivity {
             ps = new PassportService(cs);
             ps.open();
 
+            // Get the information needed for BAC, hardcoded for now
+            // TODO: link this with OCR functionality
             ps.sendSelectApplet(false);
             BACKeySpec bacKey = new BACKeySpec() {
                 @Override
-                public String getDocumentNumber() { return "NP0811B03";}
-
+                public String getDocumentNumber() {
+                    return "NP0811B03";
+                }
                 @Override
                 public String getDateOfBirth() {
                     return "940610";
                 }
-
                 @Override
-                public String getDateOfExpiry() { return "180624"; }
+                public String getDateOfExpiry() {
+                    return "180624";
+                }
             };
 
             ps.doBAC(bacKey);
-
-            InputStream is = null;
-            InputStream is15 = null;
-            InputStream isCvca = null;
-            try {
-                // Basic data
-                is = ps.getInputStream(PassportService.EF_DG1);
-                DG1File dg1 = (DG1File) LDSFileUtil.getLDSFile(PassportService.EF_DG1, is);
-
-                Toast.makeText(this, dg1.getMRZInfo().getPersonalNumber(), Toast.LENGTH_LONG).show();
-
-                // display data from dg15
-                is15 = ps.getInputStream(PassportService.EF_DG15);
-                DG15File dg15 = new DG15File(is15);
-                String publicKey = new BigInteger(dg15.getPublicKey().getEncoded()).toString(2);
-                int length = dg15.getPublicKey().getEncoded().length;
-                textView.setText(Integer.toString(length));
-
-                // sign 8 bytes of data and display the signed data
-                byte[] data = hexStringToByteArray("aaaaaaaaaaaaaaaa");
-                byte[] signedData = ps.doAA(dg15.getPublicKey(), null, null, data);
-                textSignedData.setText(byteArrayToHexString(signedData));
-
-                // CVCA
-                isCvca = ps.getInputStream(PassportService.EF_CVCA);
-                CVCAFile cvca = (CVCAFile) LDSFileUtil.getLDSFile(PassportService.EF_CVCA, isCvca);
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    is.close();
-                    is15.close();
-                    isCvca.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        } catch (CardServiceException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                ps.close();
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
+            return ps;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            ps.close();
+            Toast.makeText(this, R.string.general_error, Toast.LENGTH_LONG).show();
         }
+        return null;
+    }
 
+
+    /**
+     * Retrieves the public key used for Active Authentication from datagroup 15.
+     * @return Publickey - returns the publickey used for AA
+     */
+    private PublicKey getAAPublicKey(PassportService ps, InputStream is15) {
+        try {
+            DG15File dg15 = new DG15File(is15);
+            return dg15.getPublicKey();
+        } catch (Exception ex){
+            ex.printStackTrace();
+            Toast.makeText(this, R.string.general_error, Toast.LENGTH_LONG).show();
+        }
+        return null;
     }
 
     /**
-     * Copy pasted, because original uses explicit cast to BouncyCastle key implementation, whereas we have a spongycastle one
+     * Signs 8 bytes by the passport using the AA functionality.
+     * @return byte[] - signed byte array
      */
-    public synchronized ChipAuthenticationResult doCA(PassportService ps, BigInteger keyId, PublicKey publicKey) throws CardServiceException {
-        if (publicKey == null) { throw new IllegalArgumentException("Public key is null"); }
+    private byte[] signData(PassportService ps, InputStream is15, PublicKey pubk) {
         try {
-            String agreementAlg = Util.inferKeyAgreementAlgorithm(publicKey);
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(agreementAlg);
-            AlgorithmParameterSpec params = null;
-            if ("DH".equals(agreementAlg)) {
-                DHPublicKey dhPublicKey = (DHPublicKey)publicKey;
-                params = dhPublicKey.getParams();
-            } else if ("ECDH".equals(agreementAlg)) {
-                ECPublicKey ecPublicKey = (ECPublicKey)publicKey;
-                params = ecPublicKey.getParams();
-            } else {
-                throw new IllegalStateException("Unsupported algorithm \"" + agreementAlg + "\"");
-            }
-            keyPairGenerator.initialize(params);
-            KeyPair keyPair = keyPairGenerator.generateKeyPair();
-
-            KeyAgreement agreement = KeyAgreement.getInstance(agreementAlg);
-            agreement.init(keyPair.getPrivate());
-            agreement.doPhase(publicKey, true);
-
-            byte[] secret = agreement.generateSecret();
-
-            // TODO: this SHA1ing may have to be removed?
-            // TODO: this hashing is needed for our Java Card passport applet implementation
-            // byte[] secret = md.digest(secret);
-
-            byte[] keyData = null;
-            byte[] idData = null;
-            byte[] keyHash = new byte[0];
-            if ("DH".equals(agreementAlg)) {
-                DHPublicKey dhPublicKey = (DHPublicKey)keyPair.getPublic();
-                keyData = dhPublicKey.getY().toByteArray();
-                // TODO: this is probably wrong, what should be hashed?
-                MessageDigest md = MessageDigest.getInstance("SHA1");
-                md = MessageDigest.getInstance("SHA1");
-                keyHash = md.digest(keyData);
-            } else if ("ECDH".equals(agreementAlg)) {
-                org.spongycastle.jce.interfaces.ECPublicKey ecPublicKey = (org.spongycastle.jce.interfaces.ECPublicKey)keyPair.getPublic();
-                keyData = ecPublicKey.getQ().getEncoded();
-                byte[] t = Util.i2os(ecPublicKey.getQ().getX().toBigInteger());
-                keyHash = Util.alignKeyDataToSize(t, ecPublicKey.getParameters().getCurve().getFieldSize() / 8);
-            }
-            keyData = Util.wrapDO((byte)0x91, keyData);
-            if (keyId.compareTo(BigInteger.ZERO) >= 0) {
-                byte[] keyIdBytes = keyId.toByteArray();
-                idData = Util.wrapDO((byte)0x84, keyIdBytes);
-            }
-            ps.sendMSEKAT(ps.getWrapper(), keyData, idData);
-
-            SecretKey ksEnc = Util.deriveKey(secret, Util.ENC_MODE);
-            SecretKey ksMac = Util.deriveKey(secret, Util.MAC_MODE);
-
-            ps.setWrapper(new DESedeSecureMessagingWrapper(ksEnc, ksMac, 0L));
-            Field fld = PassportService.class.getDeclaredField("state");
-            fld.setAccessible(true);
-            fld.set(ps, 4) ; //PassportService.CA_AUTHENTICATED_STATE)
-            return new ChipAuthenticationResult(keyId, publicKey, keyHash, keyPair);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new CardServiceException(e.toString());
+            // test 8 byte string for testing purposes
+            byte[] data = hexStringToByteArray("aaaaaaaaaaaaaaaa");
+            return ps.doAA(pubk, null, null, data);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            Toast.makeText(this, R.string.general_error, Toast.LENGTH_LONG).show();
         }
-    }
-
-    public synchronized TerminalAuthenticationResult doTA(PassportService ps, CVCPrincipal caReference, List<CardVerifiableCertificate> terminalCertificates,
-                                                          PrivateKey terminalKey, String taAlg, ChipAuthenticationResult chipAuthenticationResult, String documentNumber) throws CardServiceException {
-        try {
-            if (terminalCertificates == null || terminalCertificates.size() < 1) {
-                throw new IllegalArgumentException("Need at least 1 certificate to perform TA, found: " + terminalCertificates);
-            }
-
-            byte[] caKeyHash = chipAuthenticationResult.getKeyHash();
-			/* The key hash that resulted from CA. */
-            if (caKeyHash == null) {
-                throw new IllegalArgumentException("CA key hash is null");
-            }
-
-			/* FIXME: check that terminalCertificates holds a (inverted, i.e. issuer before subject) chain. */
-
-			/* Check if first cert is/has the expected CVCA, and remove it from chain if it is the CVCA. */
-            CardVerifiableCertificate firstCert = terminalCertificates.get(0);
-            CVCAuthorizationTemplate.Role firstCertRole = firstCert.getAuthorizationTemplate().getRole();
-            if (CVCAuthorizationTemplate.Role.CVCA.equals(firstCertRole)) {
-                CVCPrincipal firstCertHolderReference = firstCert.getHolderReference();
-                if (caReference != null && !caReference.equals(firstCertHolderReference)) {
-                    throw new CardServiceException("First certificate holds wrong authority, found " + firstCertHolderReference.getName() + ", expected " + caReference.getName());
-                }
-                if (caReference == null) {
-                    caReference = firstCertHolderReference;
-                }
-                terminalCertificates.remove(0);
-            }
-            CVCPrincipal firstCertAuthorityReference = firstCert.getAuthorityReference();
-            if (caReference != null && !caReference.equals(firstCertAuthorityReference)) {
-                throw new CardServiceException("First certificate not signed by expected CA, found " + firstCertAuthorityReference.getName() + ",  expected " + caReference.getName());
-            }
-            if (caReference == null) {
-                caReference = firstCertAuthorityReference;
-            }
-
-			/* Check if the last cert is an IS cert. */
-            CardVerifiableCertificate lastCert = terminalCertificates.get(terminalCertificates.size() - 1);
-            CVCAuthorizationTemplate.Role lastCertRole = lastCert.getAuthorizationTemplate().getRole();
-            if (!CVCAuthorizationTemplate.Role.IS.equals(lastCertRole)) {
-                throw new CardServiceException("Last certificate in chain (" + lastCert.getHolderReference().getName() + ") does not have role IS, but has role " + lastCertRole);
-            }
-            CardVerifiableCertificate terminalCert = lastCert;
-
-            int i = 0;
-			/* Have the MRTD check our chain. */
-            for (CardVerifiableCertificate cert: terminalCertificates) {
-                try {
-                    CVCPrincipal authorityReference = cert.getAuthorityReference();
-
-					/* Step 1: MSE:SetDST */
-					/* Manage Security Environment: Set for verification: Digital Signature Template,
-					 * indicate authority of cert to check.
-					 */
-                    byte[] authorityRefBytes = Util.wrapDO((byte) 0x83, authorityReference.getName().getBytes("ISO-8859-1"));
-                    ps.sendMSESetDST(ps.getWrapper(), authorityRefBytes);
-
-					/* Cert body is already in TLV format. */
-                    byte[] body = cert.getCertBodyData();
-
-					/* Signature not yet in TLV format, prefix it with tag and length. */
-                    byte[] signature = cert.getSignature();
-                    ByteArrayOutputStream sigOut = new ByteArrayOutputStream();
-                    TLVOutputStream tlvSigOut = new TLVOutputStream(sigOut);
-                    tlvSigOut.writeTag(0x5F37); //TAG_CVCERTIFICATE_SIGNATURE);
-                    tlvSigOut.writeValue(signature);
-                    tlvSigOut.close();
-                    signature = sigOut.toByteArray();
-
-					/* Step 2: PSO:Verify Certificate */
-                    ps.sendPSOChainMode(ps.getWrapper(), body, signature);
-                } catch (Exception cse) {
-                    Log.w("FOO", String.valueOf(i));
-                    throw cse;
-                }
-                i++;
-            }
-
-            if (terminalKey == null) {
-                throw new CardServiceException("No terminal key");
-            }
-
-			/* Step 3: MSE Set AT */
-            CVCPrincipal holderRef = terminalCert.getHolderReference();
-            byte[] holderRefBytes = Util.wrapDO((byte) 0x83, holderRef.getName().getBytes("ISO-8859-1"));
-			/* Manage Security Environment: Set for external authentication: Authentication Template */
-            ps.sendMSESetATExtAuth(ps.getWrapper(), holderRefBytes);
-
-			/* Step 4: send get challenge */
-            byte[] rPICC = ps.sendGetChallenge(ps.getWrapper());
-
-			/* Step 5: external authenticate. */
-			/* FIXME: idPICC should be public key in case of PACE. See BSI TR 03110 v2.03 4.4. */
-            byte[] idPICC = new byte[documentNumber.length() + 1];
-            System.arraycopy(documentNumber.getBytes("ISO-8859-1"), 0, idPICC, 0, documentNumber.length());
-            idPICC[idPICC.length - 1] = (byte) MRZInfo.checkDigit(documentNumber);
-
-            ByteArrayOutputStream dtbs = new ByteArrayOutputStream();
-            dtbs.write(idPICC);
-            dtbs.write(rPICC);
-            dtbs.write(caKeyHash);
-            dtbs.close();
-            byte[] dtbsBytes = dtbs.toByteArray();
-
-            String sigAlg = terminalCert.getSigAlgName();
-            if (sigAlg == null) {
-                throw new IllegalStateException("ERROR: Could not determine signature algorithm for terminal certificate " + terminalCert.getHolderReference().getName());
-            }
-            Signature sig = Signature.getInstance(sigAlg);
-            sig.initSign(terminalKey);
-            sig.update(dtbsBytes);
-            byte[] signedData = sig.sign();
-            if (sigAlg.toUpperCase().endsWith("ECDSA")) {
-                int keySize = ((org.bouncycastle.jce.interfaces.ECPrivateKey)terminalKey).getParameters().getCurve().getFieldSize() / 8;
-                signedData = Util.getRawECDSASignature(signedData, keySize);
-            }
-            ps.sendMutualAuthenticate(ps.getWrapper(), signedData);
-            Field fld = PassportService.class.getDeclaredField("state");
-            fld.setAccessible(true);
-            fld.set(ps, 5) ; //PassportService.TA_AUTHENTICATED_STATE)
-            return new TerminalAuthenticationResult(chipAuthenticationResult, caReference, terminalCertificates, terminalKey, documentNumber, rPICC);
-        } catch (CardServiceException cse) {
-            throw cse;
-        } catch (Exception e) {
-            throw new CardServiceException(e.toString());
-        }
+        return null;
     }
 
     /**
