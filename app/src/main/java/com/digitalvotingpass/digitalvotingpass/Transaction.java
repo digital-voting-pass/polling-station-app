15464157d3f023a1929415d4eac9df5dec2fa147
meijer <wkmeijer@users.noreply.github.com>
2017-05-26 17:47:14 +0200

    added listview placeholder, actual transaction data handling needs to be added

---
+++ app/src/main/java/com/digitalvotingpass/digitalvotingpass/Transaction.java
@@ -0,0 +1,19 @@
+package com.digitalvotingpass.digitalvotingpass;
+
+import java.util.Date;
+
+/**
+ * Class used to store the transactions that are displayed in the transaction history activity.
+ * TODO: refactor this to make use of actual details of the transactions
+ */
+public class Transaction {
+    public String title;
+    public Date time;
+    public String transactionDetails;
+
+    public Transaction(String title, Date time, String transactionDetails) {
+        this.title = title;
+        this.time = time;
+        this.transactionDetails = transactionDetails;
+    }
+}
--- app/src/main/java/com/digitalvotingpass/digitalvotingpass/TransactionHistoryActivity.java
+++ app/src/main/java/com/digitalvotingpass/digitalvotingpass/TransactionHistoryActivity.java
@@ -3,20 +3,38 @@ package com.digitalvotingpass.digitalvotingpass;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class TransactionHistoryActivity extends AppCompatActivity {
+    private ListView transactionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction_history);
-
-        setContentView(R.layout.activity_transaction_history);
         Toolbar appBar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(appBar);
 
-        TextView textTransaction = (TextView) findViewById(R.id.transaction);
-        textTransaction.setText("Here a transaction is showed");
+        transactionList = (ListView) findViewById(R.id.transaction_list);
+
+        // create a transaction history array with all the transactions and add them to the list
+        // TODO: handle actual transaction history input data
+        ArrayList<Transaction> transactionHistory = new ArrayList<Transaction>();
+
+        TransactionsAdapter adapter = new TransactionsAdapter(this, transactionHistory);
+        transactionList.setAdapter(adapter);
+
+        Transaction newTransaction = new Transaction("Received voting pass", new Date(), "some details about the transaction");
+        Transaction newTransaction2 = new Transaction("Cast vote", new Date(), "some details about the transaction");
+
+        adapter.add(newTransaction);
+        adapter.add(newTransaction2);
+
     }
 }
