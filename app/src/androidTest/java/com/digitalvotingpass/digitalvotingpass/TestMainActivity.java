efc272bd5a80f2bdabdfe874768f2f1cfe5189b3
klikooo <rico12978@hotmail.com>
2017-05-29 10:59:03 +0200

    Add Android test
    Test the main activity buttons

---
+++ app/src/androidTest/java/com/digitalvotingpass/digitalvotingpass/TestMainActivity.java
@@ -0,0 +1,92 @@
+package com.digitalvotingpass.digitalvotingpass;
+
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Telephony;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.intent.Intents;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class TestMainActivity {
+
+    /**
+     * Create an Intent for each test.
+     */
+    @Rule
+    public IntentsTestRule<MainActivity> mActivity = new IntentsTestRule<MainActivity>(MainActivity.class) {
+        @Override
+        protected Intent getActivityIntent() {
+            return new Intent();
+        }
+    };
+
+
+    /**
+     * Start up the main activity for each test.
+     */
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        assertEquals("com.digitalvotingpass.digitalvotingpass", appContext.getPackageName());
+    }
+
+
+    /**
+     * Test if the manual input activity opens.
+     */
+    @Test
+    public void testGoToManual() {
+        onView(withId(R.id.manual_input_button))
+                .perform(click());
+        intended(hasComponent(ManualInputActivity.class.getName()));
+    }
+
+    /**
+     * Test if the OCR opens.
+     */
+    @Test
+    public void testGoToOCR() {
+        onView(withId(R.id.start_ocr))
+                .perform(click());
+        intended(hasComponent(CameraActivity.class.getName()));
+    }
+
+
+    /**
+     * Test if the OCR opens when there is no data available and you press start id connection.
+     */
+    @Test
+    public void testClickStartConnection() {
+        onView(withText(R.string.start_con_button))
+                .perform(click());
+        intended(hasComponent(CameraActivity.class.getName()));
+    }
+
+}
