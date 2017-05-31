da7f769c6a68caff27a4ffe562613d6f109517f7
klikooo <rico12978@hotmail.com>
2017-05-31 21:28:53 +0200

    Not only pack index.html

--- artefact.sh
+++ artefact.sh
@@ -9,7 +9,7 @@ REPO_NAME=repo
 FIXED_BRANCH=$(echo $BRANCH | sed 's/\//-/g')
 ARCHIVE=$REPO_NAME-$FIXED_BRANCH-$(date +%Y-%m-%d_%H_%M_%S)-$COMMIT.tar.bz2
 echo "Creating archive $ARCHIVE"
-tar cfj $ARCHIVE /home/travis/build/digital-voting-pass/digital-voting-pass-app/app/build/reports/androidTests/connected/index.html
+tar cfj $ARCHIVE /home/travis/build/digital-voting-pass/digital-voting-pass-app/app/build/reports/androidTests/connected/*
 FILESIZE=$(stat -c%s "$ARCHIVE")
 echo "Finished archive (size $FILESIZE), starting Google Drive upload"
 ./gdrive-linux-x64 upload --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR "$ARCHIVE"
